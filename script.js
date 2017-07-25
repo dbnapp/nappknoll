154c6740a381298734fa4b8fbb74b79a426fbc18
Daryl Napp <dbnapp@gmail.com>
2017-07-25 02:04:14 -0400

    Adding story arrow functionality

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
 jQuery(document).ready(function($) {
 
+    var page = 0;
+
     // mobile menu icon clicking
-    $('#menu-button').on('click', function(){
+    $('#menu-button').on('click', function() {
         $('body').toggleClass('nav-active');
     });
 
+
+    $('.story .page .arrow.right').on('click', function() {
+        page++;
+
+        var prevPage = $('.story .page').eq(page - 1);
+        var nextPage = $('.story .page').eq(page);
+
+        prevPage.toggleClass('shown');
+        nextPage.toggleClass('shown');
+    });
+
+    $('.story .page .arrow.left').on('click', function() {
+        page--;
+
+        var nextPage = $('.story .page').eq(page);
+        var prevPage = $('.story .page').eq(page + 1);
+
+        nextPage.toggleClass('shown');
+        prevPage.toggleClass('shown');
+    });
 });
