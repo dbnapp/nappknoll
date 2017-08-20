493ee01265a693145bd9abeecd42a07c983e7c06
Daryl Napp <dbnapp@gmail.com>
2017-08-20 02:33:04 -0400

    Adding feedback for form submission

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ jQuery(document).ready(function($) {
         $('body').toggleClass('nav-active');
     });
 
+
     // story navigation
     $('.story .page .arrow.right').on('click', function() {
         page++;
@@ -28,16 +29,25 @@ jQuery(document).ready(function($) {
         prevPage.toggleClass('shown');
     });
 
-    function showMessageSuccess(){
-        $('body').addClass('message-success');
+
+    function disableContactInputs() {
+        $("#contact-form input, #contact-form textarea").prop("disabled", "disabled");
     }
 
-    // contact form
-    $('#contact-form').on('submit', function(e){
+    // contact form submission
+    $('#contact-form').on('submit', function(e) {
+        $('body').toggleClass('popup-shown');
+        disableContactInputs();
         e.preventDefault();
-        $.post({
-            url: "https://wedding-contact-email.appspot.com",
-            data: $(this).serialize(),
-        });
+
+        // $.post({
+        //     url: "https://wedding-contact-email.appspot.com",
+        //     data: $(this).serialize(),
+        // });
+    });
+
+    // thank you popup dismissal
+    $('#popup .confirm-button').on('click', function(e) {
+        $('body').toggleClass('popup-shown');
     });
 });
